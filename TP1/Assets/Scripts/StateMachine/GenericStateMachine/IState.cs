3a28d3b10533cb254f1be2fb213134b41b5af222
philplaisir <p.montplaisir-m@hotmail.com>
2024-02-20 20:39:37 -0500

    Added state machine base with state interface, Reviewer: Étienne

---
+++ TP1/Assets/Scripts/StateMachine/GenericStateMachine/IState.cs
@@ -0,0 +1,11 @@
+
+public interface IState
+{
+    public void OnStart();
+    public void OnEnter();
+    public void OnExit();
+    public void OnUpdate();
+    public void OnFixedUpdate();
+    public bool CanEnter(IState currentState);
+    public bool CanExit();
+}
